3c635d306d026b4df21ece7ce56a20afe6000b10
theGaryLarson <gary.larson316@gmail.com>
2022-12-21 03:25:16 -0800

    loading primary stats from json file

--- src/main/java/com/anima/characters/PrimaryStatsComponent.java
+++ src/main/java/com/anima/characters/PrimaryStatsComponent.java
@@ -2,6 +2,9 @@ package com.anima.characters;
 
 import com.anima.generation.StatsMap;
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -15,20 +18,26 @@ public class PrimaryStatsComponent {
     private StatsMap<String, Integer> primaryStats;
     
     public PrimaryStatsComponent() {
-        try {
-            init();
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+        init();
     }
     
-    private void init() throws FileNotFoundException {
+    private void init(){
         
         // this implementation restricts adding new keys accidentally
         HashMap<String, Integer> readPrimaryStats = new HashMap<>();
         Gson gson = new Gson();
-        FileReader reader = new FileReader("src/main/resources/json/primaryStats.json");
+        FileReader reader;
+        try {
+            reader = new FileReader("src/main/resources/json/primaryStats.json");
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         //todo: read the primary stats from json and load into the StatsMap
+        JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+        JsonArray primaryStatsArray = jsonObject.getAsJsonArray("primaryStats");
+        for (JsonElement stat : primaryStatsArray) {
+            readPrimaryStats.put(stat.getAsString(), 0);
+        }
         primaryStats = new StatsMap<>(readPrimaryStats);
     }
     
