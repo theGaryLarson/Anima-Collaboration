acc8ff1c72f394de78261ebd10b2c2e9a1b0fe14
theGaryLarson <gary.larson316@gmail.com>
2022-12-21 02:37:55 -0800

    Gson working to parse Json files

---
+++ src/test/java/com/anima/characters/PrimaryStatsComponentTest.java
@@ -0,0 +1,11 @@
+package com.anima.characters;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimaryStatsComponentTest {
+    
+    @Test
+    void getStat() {
+        PrimaryStatsComponent stats = new PrimaryStatsComponent();
+    }
+}
