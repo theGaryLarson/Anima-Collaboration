88eb99ba5ffa239ce3ff7f7064b625cf9d15ccaf
theGaryLarson <gary.larson316@gmail.com>
2022-12-26 12:57:28 -0800

    added generalized roll method

--- src/main/java/com/anima/rules/Dice.java
+++ src/main/java/com/anima/rules/Dice.java
@@ -3,18 +3,26 @@ package com.anima.rules;
 import java.util.Random;
 
 public class Dice {
-    public static final int D4 = 4;
-    public static final int D6 = 6;
-    public static final int D8 = 8;
-    public static final int D10 = 10;
-    public static final int D12 = 12;
-    public static final int D20 = 20;
-    public static final int D100 = 100;
+    private static final int D4 = 4;
+    private static final int D6 = 6;
+    private static final int D8 = 8;
+    private static final int D10 = 10;
+    private static final int D12 = 12;
+    private static final int D20 = 20;
+    private static final int D100 = 100;
     
     private static final Random random = new Random();
     
     private Dice() {}
     
+    public static int roll(int numOfDice, int numSides) {
+        int total = 0;
+        for (int count = 0; count < numOfDice; count++) {
+            //todo use observer pattern to listen for fumble/critical rolls
+            total += random.nextInt(numSides) + 1;
+        }
+        return total;
+    }
     public static int rollD4() {
         return random.nextInt(D4) + 1;
     }
