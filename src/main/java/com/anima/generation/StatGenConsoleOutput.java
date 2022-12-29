51a4f449f92787bacdd63ff95b9e60ec8d5a9ecb
theGaryLarson <gary.larson316@gmail.com>
2022-12-28 23:08:00 -0800

    created for displaying output to the console

---
+++ src/main/java/com/anima/generation/StatGenConsoleOutput.java
@@ -0,0 +1,24 @@
+package com.anima.generation;
+
+import com.anima.base.Output;
+
+public class StatGenConsoleOutput implements Output {
+    
+    public StatGenConsoleOutput() {
+        init();
+    }
+    
+    public void init() {
+    
+    }
+    
+    @Override
+    public void writeStats() {
+    
+    }
+    
+    @Override
+    public void pickStat() {
+    
+    }
+}
